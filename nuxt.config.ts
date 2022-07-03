31b1ff451a9ed4fb3849845368f00bccc2522b2d
Qin Guan <qinguan20040914@gmail.com>
2022-07-03 13:52:08 +0800

    chore: formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,31 +4,31 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
   modules: ['nuxt-windicss'],
   typescript: {
-    strict: true
+    strict: true,
   },
   runtimeConfig: {
     jwtSecret: 'iLoveSecureSoftwareDevelopmentNgeeAnnPolytechnicGettingThatXtra',
     jwtIssuer: 'sussyland',
     jwtExpirationTime: '30m',
-    jwtCookieName: "sussyland-auth"
+    jwtCookieName: 'sussyland-auth',
   },
   app: {
     head: {
       link: [
         {
           rel: 'preconnect',
-          href: 'https://fonts.googleapis.com'
+          href: 'https://fonts.googleapis.com',
         },
         {
           rel: 'preconnect',
           href: 'https://fonts.gstatic.com',
-          crossorigin: true
+          crossorigin: true,
         },
         {
           rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,200;0,400;0,600;0,800;1,200;1,400;1,600;1,800&display=swap'
+          href: 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,200;0,400;0,600;0,800;1,200;1,400;1,600;1,800&display=swap',
         },
-      ]
-    }
-  }
+      ],
+    },
+  },
 })
