3d97c600ce502cd3a2dd112d94e01c7525d97661
Qin Guan <qinguan20040914@gmail.com>
2022-07-03 14:07:19 +0800

    fix: button size on landing page again

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -9,15 +9,15 @@ const { error, pending } = await useFetch('/api/auth/whoami', {
 <template>
   <div class="h-screen bg-background-100">
     <div class="p-3 container mx-auto font-sans">
-      <header class="my-5 flex justify-between items-center">
+      <header class="h-15 flex justify-between items-center">
         <span class="font-semibold text-lg">
           Monneh
         </span>
 
-        <SharedButton v-if="pending || error" variant="alternative" href="/auth/login" size="sm">
+        <SharedButton v-if="pending || error" variant="alternative" href="/auth/login">
           Login
         </SharedButton>
-        <SharedButton v-else href="/dashboard" size="sm">
+        <SharedButton v-else href="/dashboard">
           Dashboard
         </SharedButton>
       </header>
