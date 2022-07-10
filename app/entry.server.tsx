cb97a52674fe019cf55eadeb0616f7d262ba612b
Qin Guan <qinguan20040914@gmail.com>
2022-07-10 17:58:40 +0800

    feat(v2): add mantine

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,7 @@
-import type { EntryContext } from "@remix-run/node";
-import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
+import {renderToString} from 'react-dom/server';
+import {RemixServer} from '@remix-run/react';
+import type {EntryContext} from '@remix-run/node';
+import {injectStylesIntoStaticMarkup} from '@mantine/ssr';
 
 export default function handleRequest(
   request: Request,
@@ -8,13 +9,10 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  let markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  );
+  const markup = renderToString(<RemixServer context={remixContext} url={request.url}/>);
+  responseHeaders.set('Content-Type', 'text/html');
 
-  responseHeaders.set("Content-Type", "text/html");
-
-  return new Response("<!DOCTYPE html>" + markup, {
+  return new Response(`<!DOCTYPE html>${injectStylesIntoStaticMarkup(markup)}`, {
     status: responseStatusCode,
     headers: responseHeaders,
   });
