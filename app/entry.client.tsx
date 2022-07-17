247b074cc96c4b15fe90508246c7fdf718d99743
Qin Guan <qinguan20040914@gmail.com>
2022-07-17 22:45:37 +0800

    fix(v2): hydration

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,9 +1,4 @@
 import { RemixBrowser } from '@remix-run/react'
 import { hydrateRoot } from 'react-dom/client'
-import { startTransition } from 'react'
 
-requestIdleCallback(() => {
-    startTransition(() => {
-        hydrateRoot(document, <RemixBrowser />)
-    })
-})
+hydrateRoot(document, <RemixBrowser />)
