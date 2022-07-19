080b93a1f254fb8b2d8b8daa01ee20105486a461
Qin Guan <qinguan20040914@gmail.com>
2022-07-19 23:04:24 +0800

    fix(v2): revert to react 17
    document hydration doesn't work with extensions

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,4 @@
 import { RemixBrowser } from '@remix-run/react'
-import { hydrateRoot } from 'react-dom/client'
-import { startTransition } from 'react'
+import { hydrate } from 'react-dom'
 
-startTransition(() => {
-    hydrateRoot(document, <RemixBrowser />)
-})
+hydrate(<RemixBrowser />, document)
