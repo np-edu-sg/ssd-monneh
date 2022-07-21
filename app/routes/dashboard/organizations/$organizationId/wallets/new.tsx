c2aa67eb1771e70fcb9f3ce091bb0236c449f53c
Qin Guan <qinguan20040914@gmail.com>
2022-07-21 19:18:38 +0800

    fix(v2): missing input field

--- app/routes/dashboard/organizations/$organizationId/wallets/new.tsx
+++ app/routes/dashboard/organizations/$organizationId/wallets/new.tsx
@@ -150,6 +150,7 @@ export default function NewWalletPage() {
                                       )
                                     : '$ '
                             }
+                            {...form.getInputProps('balance')}
                         />
                     </SimpleGrid>
 
